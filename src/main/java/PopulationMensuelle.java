d799afc8ee34a50a2b58ff851e1fab8f21acf0d6
Bastien QUIN <baquin@local.isima.fr>
2021-10-25 16:44:12 +0200

    [+] Ajout PopulationMensuelle

--- src/main/java/PopulationMensuelle.java
+++ src/main/java/PopulationMensuelle.java
@@ -1,6 +1,21 @@
+package java;
+
 import org.junit.jupiter.api.Test;
 
 public class PopulationMensuelle {
+
+    int age;
+    int rabbitNumber;
+    int aliveRabbitNumber;
+    int femaleNumber;
+    int monthsMaturity;
+    int littersNumber;
+
+    public void evolution(Population population) {
+
+
+
+    }
 
 
 
