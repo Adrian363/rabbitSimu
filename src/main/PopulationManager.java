4741719ba0def3030eda0866c87714a8ea2e9ae0
Adrian COTTAIS <adrian.cottais@etu.uca.fr>
2021-11-15 15:20:28 +0100

    [+] Some modifs

--- src/main/PopulationManager.java
+++ src/main/PopulationManager.java
@@ -7,7 +7,7 @@ public class PopulationManager {
         int[][] possibleLitters = { { 4, 5 }, { 5, 30 }, { 6, 30 }, { 7, 30 }, { 8, 5 } };
         int yearsSimulation = 10;
 
-        Population rabbitPopulation = new Population(11,11,3,6,50,4,8,60,35,10,10,possibleLitters);
+        Population rabbitPopulation = new Population(10,10, 3,6,50,4,8,60,35,10,10,possibleLitters);
 
         /*
         for (int i = 0 ; i < yearsSimulation * 12 ; i++) {
